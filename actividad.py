d14e996fc4bb45ecb13ab224d41e4ed1f5efbff6
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-19 20:46:15 -0600

    Simulación de Votación terminado

--- actividad.py
+++ actividad.py
@@ -3,17 +3,19 @@
 name= input("Ingrese su nombre completo:")
 name_conteo= len(name)
 dpi= input("Ingrese su número de DPI:")
-dpiconteo=len(dpi)
-departamento= input("Ingrese nombre de departamento:")
+dpi=len(dpi)
+departamento= input("Ingrese nombre de departamento:").lower()
 año_nacimiento = int(input("Ingrese año de nacimiento:"))
-
 edad_año=  2025 - año_nacimiento
+excepcion_departamento= ["Peten", "Alta Verapaz"]
 
-if edad_año < 18:
-    print("No es mayor de edad. No puede votar")
+if edad_año < 18 and not (departamento in excepcion_departamento and edad_año ==17):
+    print("No cumple con edad requerida. No puede votar")
 
-if name_conteo <5:
+elif name_conteo <5:
     print("Su nombre debe poseer mas de 5 letras")
 
-if (dpi > 13) and (dpi < 13):
+elif (dpi > 13) or (dpi < 13):
     print("DPI invalido, debe tener 13 dígitos exactamente.")
+else:
+    print(f"Bienvenido {name},su centro de votación es {departamento.capitalize()}")
