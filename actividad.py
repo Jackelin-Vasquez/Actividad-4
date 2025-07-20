e83a1e8880be051d1e3ba438499768d939fc3246
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-20 00:16:18 -0600

    calculadora de Impuestos 2.0

--- actividad.py
+++ actividad.py
@@ -27,16 +27,22 @@ else:
 annueal_income= float(input("Ingrese el ingreso anual:"))
 dependents_number= int(input("Ingrese número de dependientes:"))
 dependents_deduction= 1000 * dependents_number
+deducted_income= annueal_income - dependents_deduction
 
 
 if annueal_income < 40000 and dependents_number > 2:
     percentage= 0
 
-elif annueal_income >=0 and annueal_income <=30000:
-    percentage= (annueal_income*5)/100
-elif annueal_income >=30001 and annueal_income <=60000:
-    percentage= (annueal_income*10)/100
-elif annueal_income >= 60001 and annueal_income <=100000:
-    percentage= (annueal_income*20)/100
-
-deducted_income= annueal_income - dependents_deduction
+else:
+    if deducted_income <=30000:
+        percentage= (deducted_income*5)/100
+    elif deducted_income <=60000:
+        percentage= (deducted_income*10)/100
+    elif deducted_income <=100000:
+        percentage= (deducted_income*15)/100
+    elif deducted_income > 100000:
+        percentage= (deducted_income *20)/100
+total_income= annueal_income - percentage
+print(f"Su ingreso anual es de:{annueal_income} \n Monto dedudido por dependientes {dependents_deduction}")
+print(f"Ingreso despues de deducciones:{deducted_income}")
+print(f"Ingreso despues de impuestos: {total_income} \n Impuesto toal a pagar:{percentage}")
