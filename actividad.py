1ea11dc89b7921594226192f7305547b4653a446
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-20 22:33:58 -0600

    Simulador de entradas al cine

--- actividad.py
+++ actividad.py
@@ -265,6 +265,11 @@ else:
 age= int(input("Ingrese su edad:"))
 day_week = input("Ingrese día de la semana:").lower()
 student= input("¿es estudiante:").lower()
+week= ["Lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+if day_week in week:
+    week= True
+else:
+    print("Día inexistente")
 
 if age < 13:
     print("No puede ver peliculas para mayores de 15 años")
